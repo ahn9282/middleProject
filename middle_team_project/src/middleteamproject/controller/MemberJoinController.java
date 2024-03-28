d21c10f24e1fe791b080c5cefe4a5eca79aed68a
cajunSub <junsyub@gmail.com>
2024-03-28 17:28:36 +0900

    간단 정리

--- middle_team_project/src/middleteamproject/controller/MemberJoinController.java
+++ middle_team_project/src/middleteamproject/controller/MemberJoinController.java
@@ -15,14 +15,13 @@ public class MemberJoinController implements Controller{
 	public String process(HttpServletRequest request, HttpServletResponse reponse) {
 	 
 		String joinId = request.getParameter("userID");
-		System.out.println("입력한 id :  "+joinId);
+		
 		MemberDAO dao = new MemberDAO();
 		List<String> list = dao.listID();
 		for (String string : list) {
-			System.out.println(string + "||");
-		}
+		
 		int existId = list.indexOf(joinId);
-		System.out.println("중복id확인 : " + existId);
+		
 		if(existId >= 0) {
 			request.setAttribute("existingId", true);
 		}
